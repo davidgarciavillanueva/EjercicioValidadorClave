e99cf5202c20eedd85e0bd24cf4b2cd0a498d7b3
david gracia <davidgraciavillanueva15@gmail.com>
2025-04-30 13:34:03 -0500

    add metodo de de que tenga numero y el abs

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -15,6 +15,14 @@ class ReglaValidacion(ABC):
     def _contiene_minuscula(self, clave: str) -> bool:
         return any(c.islower() for c in clave)
 
+    def _contiene_numero(self, clave: str) -> bool:
+        return any(c.isdigit() for c in clave)
+
+    @abstractmethod
+    def es_valida(self, clave: str) -> bool:
+        pass
+
+
 
 
 class Validador:
