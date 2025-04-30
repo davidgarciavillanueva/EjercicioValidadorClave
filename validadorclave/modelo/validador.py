ffc24ed59b86df1cd5214ed3d88888eb133d44ff
david gracia <davidgraciavillanueva15@gmail.com>
2025-04-30 13:30:30 -0500

    add metodo de de que tenga mayusculas y minusculas

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,5 +1,23 @@
 from abc import ABC, abstractmethod
 
+
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada:int ):
+        self._longitud_esperada = None
+        self.longitud_esperada=longitud_esperada
+
+    def _validar_longitud(self, clave: str) -> bool:
+        return len(clave) > self._longitud_esperada
+
+    def _contiene_mayuscula(self, clave: str) -> bool:
+        return any(c.isupper() for c in clave)
+
+    def _contiene_minuscula(self, clave: str) -> bool:
+        return any(c.islower() for c in clave)
+
+
+
 class Validador:
     pass
 
@@ -8,11 +26,3 @@ class ReglaValidacionGanimedes:
 
 class ReglaValidacionCalisto:
     pass
-
-class ReglaValidacion(ABC):
-    def __init__(self, longitud_esperada:int ):
-        self.longitud_esperada=longitud_esperada
-
-    def _validar_longitud(self, clave: str) -> bool:
-        return len(clave) > self._longitud_esperada
-
