525a4b5aaa5ea2ff082b2cc424e0f020e1e433ad
andreamc230 <marincontaduria@gmail.com>
2025-04-30 13:18:46 -0500

    add clase regla de validacion ABS

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,1 +1,14 @@
-# TODO: Implementa el código del ejercicio aquí
+from abc import ABC, abstractmethod
+
+class Validador:
+    pass
+
+class ReglaValidacionGanimedes:
+    pass
+
+class ReglaValidacionCalisto:
+    pass
+
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada:int ):
+        self.longitud_esperada=longitud_esperada
